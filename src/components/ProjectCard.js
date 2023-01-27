5a6be0a7878d87a56b048284c4d938725471fb59
William Shaner <williamshaner3@gmail.com>
2023-01-27 17:09:19 -0600

    commented out some projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
 import { SiGithub } from 'react-icons/si';
 import '../style/ProjectCard/ProjectCard.css';
 
 function ProjectCard({ project, onclick }) {
-  const [overlayShow, setOverlayShow] = useState(false);
   const {
     title, link, image, github, id,
   } = project;
   const { ref, inView } = useInView({ threshold: 0.3 });
   return (
     <div
-      onMouseEnter={() => setOverlayShow(true)}
-      onMouseLeave={() => setOverlayShow(false)}
       key={id}
       ref={ref}
       style={{ width: '18rem' }}
@@ -21,25 +18,18 @@ function ProjectCard({ project, onclick }) {
     >
       <img src={image} alt="project" />
 
-      { overlayShow && (
-        <>
-          <div className={`overlay-container ${overlayShow && 'fade-in-container'}`} />
-
-          <div className={`overlay-content ${overlayShow && 'fade-in-content'}`}>
-            <h4>{title}</h4>
-            <div className="overlay-content-buttons">
-              <a href={link} target="_blank" rel="noreferrer">
-                Live Site
-              </a>
-              <a href={github} target="_blank" rel="noreferrer">
-                <SiGithub />
-              </a>
-              <Link aria-label={`Read more about ${title}`} to="/projects" onClick={onclick}>Read More</Link>
-            </div>
-          </div>
-        </>
-      )}
-
+      <div className="overlay-content">
+        <h4>{title}</h4>
+        <div className="overlay-content-buttons">
+          <a href={link} target="_blank" rel="noreferrer">
+            Live Site
+          </a>
+          <a href={github} target="_blank" rel="noreferrer">
+            <SiGithub />
+          </a>
+          <Link aria-label={`Read more about ${title}`} to="/projects" onClick={onclick}>Read More</Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,8 @@
-import project1 from '../assets/mathGame.webp';
+/* eslint-disable max-len */
+// import project1 from '../assets/mathGame.webp';
 import bikeShop from '../assets/bikeShop.webp';
-import toDoList from '../assets/toDoList.webp';
-import appleClone from '../assets/appleClone.webp';
+import toDoList from '../assets/to-do-list.png';
+// import appleClone from '../assets/appleClone.webp';
 import weather from '../assets/weatherApp.webp';
 import nbaStats from '../assets/nbaStats.webp';
 
@@ -33,25 +34,25 @@ const PROJECTS = [
     description: 'This weather app was created using React, Bootstrap, and the Open Weather API. This project was mainly to showcase my skills working with RESTful APIs. The project began with gathering the data from API, chose which data is useful, and layout the data in a way that was user friendly. The application is completely responsive to all screen sizes and includes a refresh button using the application at different locations.',
     background: 'I built this project to work on my skills working with RESTful APIs. I used the Fetch API to make the server request. I got the background image from Unsplash and created a container that held all the data that the API provided.',
   },
-  {
-    id: 3,
-    title: 'Apple Clone',
-    skills: ['Bootstrap', 'HTML', 'CSS', 'Javascript', 'GSAP'],
-    link: 'https://dynamic-travesseiro-58dba1.netlify.app/',
-    image: appleClone,
-    github: 'https://github.com/WillShaner/AppleClone',
-    description: 'This Apple clone was created using the Javascript, HTML, CSS, Bootstrap, and GSAP. This project was to showcase and work on my skills using GreenSock Animation Platform. I used GSAP to create the screen pin on the scroll of the images. This was also an opportunity to showcase my skills working with Bootstrap as this is a fully responsive site.',
-    background: 'This was my first site created in Altcademy`s front-end program. I came back to the project several months later and was able to make it fully responsive and creating a navbar that changes on  screen size.',
-  },
+  // {
+  //   id: 3,
+  //   title: 'Apple Clone',
+  //   skills: ['Bootstrap', 'HTML', 'CSS', 'Javascript', 'GSAP'],
+  //   link: 'https://dynamic-travesseiro-58dba1.netlify.app/',
+  //   image: appleClone,
+  //   github: 'https://github.com/WillShaner/AppleClone',
+  //   description: 'This Apple clone was created using the Javascript, HTML, CSS, Bootstrap, and GSAP. This project was to showcase and work on my skills using GreenSock Animation Platform. I used GSAP to create the screen pin on the scroll of the images. This was also an opportunity to showcase my skills working with Bootstrap as this is a fully responsive site.',
+  //   background: 'This was my first site created in Altcademy`s front-end program. I came back to the project several months later and was able to make it fully responsive and creating a navbar that changes on  screen size.',
+  // },
   {
     id: 4,
     title: 'To Do List',
-    skills: ['Bootstrap', 'HTML', 'CSS', 'Javascript', 'REST API'],
+    skills: ['HTML', 'CSS', 'Javascript', 'REST API'],
     link: 'https://fantastic-tapioca-49393c.netlify.app/',
     image: toDoList,
     github: 'https://github.com/WillShaner/To-Do-List',
-    description: 'This to do list was created using HTML, CSS, JQuery, and a RESTful API. This project was to showcase my skills interacting with a RESTful API. The user can add tasks, delete tasks, and mark the tasks complete. The user can filter the tasks between completed and active tasks. The user can also toggle between light and dark mode.',
-    background: 'This project was created as part of Altcademy`s frontend program. The site uses Altcademy`s API to GET, POST, DELETE, and PUT tasks. The background is a changes on every load from Unsplash.',
+    description: 'This to do list was created using HTML, CSS, JQuery, and a several RESTful APIs. This project was to showcase my skills interacting with a RESTful API. The user can add tasks, delete tasks, and mark the tasks complete. The user can filter the tasks between completed and active tasks. The user can also toggle between light and dark mode.',
+    background: 'This project was created as part of Altcademy`s frontend program. The site uses Altcademy`s API to GET, POST, DELETE, and PUT tasks. The project has been updated and is now more of a productivity app with the current weather, a motivational quote, and a to-do list.',
   },
   {
     id: 5,
@@ -63,15 +64,15 @@ const PROJECTS = [
     description: 'This project was built using Next.js along with Bootstrap. The site includes a home  page and a search page in which a user can search for any NBA player since 1979. The user can then see their season averages and can then search for season averages by a different season.',
     background: 'This project was built to practice my skills and better learn Next.js. I learned about the file structure that Next.js uses and the benefits of Next.js for performance. The app fetches data from the balldontlie API.',
   },
-  {
-    id: 6,
-    title: '10 Second Math Game',
-    skills: ['Bootstrap', 'HTML', 'CSS', 'Javascript'],
-    link: 'https://frolicking-kelpie-d4579c.netlify.app',
-    image: project1,
-    github: 'https://github.com/WillShaner/10-Second-Math-Game',
-    description: 'This project was built using JQuery, HTML, and CSS. The project is a game where a user has 10 seconds to answer as many questions as possible before the timer is up. If the user answers correctly then time is added to the clock and points to the score. The user has the option to choose which kind of math questions they would like to answer and the range of numbers the questions should be in. The higher the numbers the more points added per questions answered correctly. The high score of the game is stored in Altcademy`s RESTful API and is updated if the user beats the high score.',
-    background: 'The goal of this project was to work on my javascript login and problem solving. There was a lot of time spent configuring the questions so that they appear with the larger number first and divide evenly for the division problems. I made the design of the project myself.',
-  },
+  // {
+  //   id: 6,
+  //   title: '10 Second Math Game',
+  //   skills: ['Bootstrap', 'HTML', 'CSS', 'Javascript'],
+  //   link: 'https://frolicking-kelpie-d4579c.netlify.app',
+  //   image: project1,
+  //   github: 'https://github.com/WillShaner/10-Second-Math-Game',
+  //   description: 'This project was built using JQuery, HTML, and CSS. The project is a game where a user has 10 seconds to answer as many questions as possible before the timer is up. If the user answers correctly then time is added to the clock and points to the score. The user has the option to choose which kind of math questions they would like to answer and the range of numbers the questions should be in. The higher the numbers the more points added per questions answered correctly. The high score of the game is stored in Altcademy`s RESTful API and is updated if the user beats the high score.',
+  //   background: 'The goal of this project was to work on my javascript login and problem solving. There was a lot of time spent configuring the questions so that they appear with the larger number first and divide evenly for the division problems. I made the design of the project myself.',
+  // },
 ];
 export default PROJECTS;
